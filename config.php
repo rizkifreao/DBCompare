7a638db651808712557d344ef2391faa029afa5e
Rizkifreao <rizkipebrianto96@gmail.com>
2021-02-16 09:44:46 +0700

    update

--- config.php
+++ config.php
@@ -1,30 +1,18 @@
 <?php
 
-/**
- *
- * Configurasi Variabel yang dibutuhkan
- *
- * 
- * @package   DBCOMPARE
- * @author    Rizki Pebrianto <rizkipebrianto96@gmail.com>
- * @param     ...
- * @return    ...
- *
- */
-
-define('ROOT_DIR', dirname(__FILE__));
-define('ENV_FILE', ROOT_DIR . '/.env');
-define('DRIVER_DIR', ROOT_DIR . '/driver/');
-define('TEMPLATE_DIR', ROOT_DIR . '/template/');
+define('DIR_ROOT', dirname(__FILE__));
+define('ENVIRONMENT_FILE', DIR_ROOT . '/.env');
+define('DRIVER_DIR', DIR_ROOT . '/driver/');
+define('TEMPLATE_DIR', DIR_ROOT . '/template/');
 
 if (isset($_ENV['DATABASE_DRIVER']) && isset($_ENV['DATABASE_HOST']) && isset($_ENV['DATABASE_HOST_SECONDARY'])) {
   $params = $_ENV;
 } else {
-  if (!file_exists(ENV_FILE)) die('File "' . ENV_FILE . '" tidak ada. Buat file gann..');
-  $params = parse_ini_file(ENV_FILE, false, INI_SCANNER_RAW);
+  if (!file_exists(ENVIRONMENT_FILE)) die('File "' . ENVIRONMENT_FILE . '" not exist. Please create file.');
+  $params = parse_ini_file(ENVIRONMENT_FILE, false, INI_SCANNER_RAW);
 }
 
-$_paramEnv = array(
+$requiredParams = array(
   'DATABASE_DRIVER',
   'DATABASE_ENCODING',
   'SAMPLE_DATA_LENGTH',
@@ -46,13 +34,11 @@ $_paramEnv = array(
 
 array_map(function ($name) use ($params) {
   if (!isset($params[$name])) {
-    die('Atribut ' . $name . ' tidak ditemukan di file ' . ENV_FILE);
+    die('Param ' . $name . ' not set in file ' . ENVIRONMENT_FILE);
   } else {
     define($name, $params[$name]);
   }
-}, $_paramEnv);
+}, $requiredParams);
 
-define('DEV_DSN',  DATABASE_DRIVER . '://' . DATABASE_USER . ':' . DATABASE_PASSWORD . '@' . DATABASE_HOST . ':' . DATABASE_PORT . '/' . DATABASE_NAME);
-define('PROD_DSN',  DATABASE_DRIVER . '://' . DATABASE_USER_SECONDARY . ':' . DATABASE_PASSWORD_SECONDARY . '@' . DATABASE_HOST_SECONDARY . ':' . DATABASE_PORT_SECONDARY . '/' . DATABASE_NAME_SECONDARY);
-
-/* End of file compare.php */
+define('FIRST_DSN',  DATABASE_DRIVER . '://' . DATABASE_USER . ':' . DATABASE_PASSWORD . '@' . DATABASE_HOST . ':' . DATABASE_PORT . '/' . DATABASE_NAME);
+define('SECOND_DSN',  DATABASE_DRIVER . '://' . DATABASE_USER_SECONDARY . ':' . DATABASE_PASSWORD_SECONDARY . '@' . DATABASE_HOST_SECONDARY . ':' . DATABASE_PORT_SECONDARY . '/' . DATABASE_NAME_SECONDARY);
